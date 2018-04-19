1ec5b3ad724beb095080d50d9aac1801be14908a
Ryoma Scott <36080724+ryoma1@users.noreply.github.com>
2018-04-19 11:48:06 -0400

    yes

--- splash_screen.lua
+++ splash_screen.lua
@@ -25,20 +25,20 @@ local scene = composer.newScene( sceneName )
 -----------------------------------------------------------------------------------------
  
 -- The local variables for this scene
-local beetleship
+local kim
 local scrollXSpeed = 8
 local scrollYSpeed = -3
-local jungleSounds = audio.loadSound("Sounds/animals144.mp3")
-local jungleSoundsChannel
+local blipSounds = audio.loadSound("Sounds/blip.mp3")
+local blipSoundsChannel
 
 --------------------------------------------------------------------------------------------
 -- LOCAL FUNCTIONS
 --------------------------------------------------------------------------------------------
 
--- The function that moves the beetleship across the screen
-local function moveBeetleship()
-    beetleship.x = beetleship.x + scrollXSpeed
-    beetleship.y = beetleship.y + scrollYSpeed
+-- The function that moves kim across the screen
+local function moveKim()
+    kim.x = kim.x + scrollXSpeed
+    kim.y = kim.y + scrollYSpeed
 end
 
 -- The function that will go to the main menu 
@@ -59,15 +59,15 @@ function scene:create( event )
     -- set the background to be black
     display.setDefault("background", 0, 0, 0)
 
-    -- Insert the beetleship image
-    beetleship = display.newImageRect("Images/beetleship.png", 200, 200)
+    -- Insert kims image
+    kim = display.newImageRect("Images/kim.png", 200, 200)
 
     -- set the initial x and y position of the beetleship
-    beetleship.x = 100
-    beetleship.y = display.contentHeight/2
+    kim.x = 100
+    kim.y = display.contentHeight/2
 
     -- Insert objects into the scene group in order to ONLY be associated with this scene
-    sceneGroup:insert( beetleship )
+    sceneGroup:insert( kim )
 
 end -- function scene:create( event )
 
@@ -92,7 +92,7 @@ function scene:show( event )
 
     elseif ( phase == "did" ) then
         -- start the splash screen music
-        jungleSoundsChannel = audio.play(jungleSounds )
+        blipSoundsChannel = audio.play(blipSounds )
 
         -- Call the moveBeetleship function as soon as we enter the frame.
         Runtime:addEventListener("enterFrame", moveBeetleship)
@@ -126,7 +126,7 @@ function scene:hide( event )
     elseif ( phase == "did" ) then
         
         -- stop the jungle sounds channel for this screen
-        audio.stop(jungleSoundsChannel)
+        audio.stop(blipSoundsChannel)
     end
 
 end --function scene:hide( event )
