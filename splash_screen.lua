22f490bd196d09d0fea0160f5f34c0181a9c48de
Ryoma Scott <36080724+ryoma1@users.noreply.github.com>
2018-05-03 11:16:02 -0400

    yes

--- splash_screen.lua
+++ splash_screen.lua
@@ -25,7 +25,7 @@ local scene = composer.newScene( sceneName )
 -----------------------------------------------------------------------------------------
  
 -- The local variables for this scene
-local kim
+local bird
 local scrollXSpeed = 8
 local scrollYSpeed = -3
 local blipSounds = audio.loadSound("Sounds/blip.mp3")
@@ -35,10 +35,10 @@ local blipSoundsChannel
 -- LOCAL FUNCTIONS
 --------------------------------------------------------------------------------------------
 
--- The function that moves kim across the screen
-local function moveKim()
-    kim.x = kim.x + scrollXSpeed
-    kim.y = kim.y + scrollYSpeed
+-- The function that moves the bird across the screen
+local function moveBird()
+    bird.x = bird.x + scrollXSpeed
+    bird.y = bird.y + scrollYSpeed
 end
 
 -- The function that will go to the main menu 
@@ -59,15 +59,15 @@ function scene:create( event )
     -- set the background to be black
     display.setDefault("background", 0, 0, 0)
 
-    -- Insert kims image
-    kim = display.newImageRect("Images/kim.png", 200, 200)
+    -- Insert the bird image
+    bird = display.newImageRect("Images/CompanyLogoRyoma.png", 200, 200)
 
-    -- set the initial x and y position of the beetleship
-    kim.x = 100
-    kim.y = display.contentHeight/2
+    -- set the initial x and y position of the bird
+    bird.x = 100
+    bird.y = display.contentHeight/2
 
     -- Insert objects into the scene group in order to ONLY be associated with this scene
-    sceneGroup:insert( kim )
+    sceneGroup:insert( bird )
 
 end -- function scene:create( event )
 
@@ -95,7 +95,7 @@ function scene:show( event )
         blipSoundsChannel = audio.play(blipSounds )
 
         -- Call the moveBeetleship function as soon as we enter the frame.
-        Runtime:addEventListener("enterFrame", moveBeetleship)
+        Runtime:addEventListener("enterFrame", moveBird)
 
         -- Go to the main menu screen after the given time.
         timer.performWithDelay ( 3000, gotoMainMenu)          
